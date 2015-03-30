f1ff0bfd00491a8a287fdffe3a0f9b1a1b5c7b75
ihptru <ihptru@gmail.com>
2015-03-30 09:19:49 +0300

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.intro-fotorama').fotorama({
+		height: '350px',
+		width: '100%',
+		fit: 'cover',
+		nav: false,
+		arrows: false,
+		autoplay: 5000,
+		transition: 'dissolve',
+		transitionduration: 1000
+	});
+});
